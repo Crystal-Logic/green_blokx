a9517eaa2b0f0f3bcaa74b272afd9830637d2bd5
“Nadiya <nadiya.sidelnyk@crystalogic.net>
2022-11-21 01:12:43 +0200

    add construction view

---
+++ components/Construction/Construction.tsx
@@ -0,0 +1,22 @@
+import { Box, Heading, Text, Image } from '@chakra-ui/react';
+
+export const Construction = () => (
+  <Box minH={'100vh'}>
+    <Box w={'full'} pl={205} mt={88} mb={41}>
+      <Heading as="h2" fontSize="32px" lineHeight="40px" fontWeight="800">
+        Intelligent Construction Design for a{' '}
+        <Text as={'span'} color="brand.green">
+          Greener Future
+        </Text>
+      </Heading>
+      <Text fontSize="22px" fontWeight="500" lineHeight="28px">
+        Tokenized{' '}
+        <Text as={'span'} color="brand.green">
+          Eco Panel
+        </Text>{' '}
+        Manufacturing
+      </Text>
+    </Box>
+    <Image src="/images/video_placeholder.png" width={'full'} alt="construction video" />
+  </Box>
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { Box } from '@chakra-ui/react';
 
 import { Main } from '../components/Main';
+import { Construction } from '../components/Construction';
 
 const IndexPage = () => {
   return (
     <Box>
       <Main />
+      <Construction />
     </Box>
   );
 };
