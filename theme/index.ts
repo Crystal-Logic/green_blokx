5d9c3aa835777f90db9b0cdfd0f84eafcbff4043
“Nadiya <nadiya.sidelnyk@crystalogic.net>
2022-11-20 02:00:52 +0200

    add chakra

---
+++ theme/index.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const font = '"Orbitron", sans-serif';
+
+export const theme = extendTheme({
+  colors: {},
+  fonts: {
+    heading: font,
+    body: font,
+  },
+});
