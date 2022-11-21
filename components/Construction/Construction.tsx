097dc6798c1742013c7f3c6474f11399052c28be
“Nadiya <nadiya.sidelnyk@crystalogic.net>
2022-11-21 02:47:54 +0200

    fix

--- components/Construction/Construction.tsx
+++ components/Construction/Construction.tsx
@@ -2,7 +2,7 @@ import { Box, Heading, Text, Image } from '@chakra-ui/react';
 
 export const Construction = () => (
   <Box minH={'100vh'}>
-    <Box w={'full'} pl={205} mt={88} mb={41}>
+    <Box w={'full'} pl={205} pt={88} pb={41}>
       <Heading as="h2" fontSize="32px" lineHeight="40px" fontWeight="800">
         Intelligent Construction Design for a{' '}
         <Text as={'span'} color="brand.green">
