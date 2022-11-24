430f627ce061a63b1a5387e6839ebecf1bd5c7ff
“Nadiya <nadiya.sidelnyk@crystalogic.net>
2022-11-24 20:12:42 +0200

    add brand popovers

--- components/Partners/Partners.tsx
+++ components/Partners/Partners.tsx
@@ -1,4 +1,48 @@
-import { Box, Heading, Flex, Image, useColorModeValue } from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  Flex,
+  Image,
+  useColorModeValue,
+  Text,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  Link,
+} from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
+
+const LinkCard = ({ href }: any) => {
+  const popoverColor = useColorModeValue('white', 'brand.dark');
+
+  return (
+    <PopoverContent
+      border={'1px solid'}
+      borderColor={'brand.green'}
+      borderRadius={'none'}
+      w={'290px'}
+      h={'130px'}
+      bg={popoverColor}
+      p={'30px'}
+    >
+      <Text fontSize="20px" lineHeight="25px" fontWeight="500">
+        Company
+      </Text>
+      <Link
+        href={href}
+        fontSize="20px"
+        lineHeight="25px"
+        fontWeight="500"
+        color={'brand.red'}
+        textDecorationLine="underline"
+        mt={'18px'}
+        isExternal
+      >
+        Go to the website <ExternalLinkIcon w={'21px'} h={'21px'} mx="2px" />
+      </Link>
+    </PopoverContent>
+  );
+};
 
 export const Partners = () => {
   const gridColor = useColorModeValue('brand.green', 'white');
@@ -22,13 +66,18 @@ export const Partners = () => {
           <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
         </Flex>
         <Flex alignItems={'center'}>
-          <Image
-            src={'/images/partners/inspira_wealth_logo.png'}
-            alt="inspira wealth logo"
-            w={'249px'}
-            h={'68px'}
-            mx={'40px'}
-          />
+          <Popover trigger="hover" placement="auto">
+            <PopoverTrigger>
+              <Image
+                src={'/images/partners/inspira_wealth_logo.png'}
+                alt="inspira wealth logo"
+                w={'249px'}
+                h={'68px'}
+                mx={'40px'}
+              />
+            </PopoverTrigger>
+            <LinkCard href={'https://www.inspirawealth.com/'} />
+          </Popover>
         </Flex>
         <Flex flex={'1'} h={'full'} alignItems={'center'}>
           <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
@@ -42,7 +91,12 @@ export const Partners = () => {
           <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
         </Flex>
         <Flex alignItems={'center'}>
-          <Image src={'/images/partners/aurora_logo.png'} alt="aurora logo" w={'135px'} h={'90px'} mx={'40px'} />
+          <Popover trigger="hover" placement="auto">
+            <PopoverTrigger>
+              <Image src={'/images/partners/aurora_logo.png'} alt="aurora logo" w={'135px'} h={'90px'} mx={'40px'} />
+            </PopoverTrigger>
+            <LinkCard href={'https://www.inspirawealth.com/'} />
+          </Popover>
         </Flex>
         <Flex w={'546px'} h={'full'} alignItems={'center'}>
           <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
@@ -56,7 +110,18 @@ export const Partners = () => {
           <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
         </Flex>
         <Flex alignItems={'center'}>
-          <Image src={'/images/partners/moonboots_logo.png'} alt="moonboots logo" w={'260px'} h={'45px'} mx={'40px'} />
+          <Popover trigger="hover" placement="auto">
+            <PopoverTrigger>
+              <Image
+                src={'/images/partners/moonboots_logo.png'}
+                alt="moonboots logo"
+                w={'260px'}
+                h={'45px'}
+                mx={'40px'}
+              />
+            </PopoverTrigger>
+            <LinkCard href={'https://www.inspirawealth.com/'} />
+          </Popover>
         </Flex>
         <Flex w={'20px'} h={'full'} alignItems={'center'}>
           <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
@@ -70,7 +135,12 @@ export const Partners = () => {
           <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
         </Flex>
         <Flex alignItems={'center'}>
-          <Image src={'/images/partners/near_logo.png'} alt="near logo" w={'215px'} h={'84px'} mx={'40px'} />
+          <Popover trigger="hover" placement="auto">
+            <PopoverTrigger>
+              <Image src={'/images/partners/near_logo.png'} alt="near logo" w={'215px'} h={'84px'} mx={'40px'} />
+            </PopoverTrigger>
+            <LinkCard href={'https://near.org/'} />
+          </Popover>
         </Flex>
         <Flex flex={'1'} h={'full'} alignItems={'center'}>
           <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
