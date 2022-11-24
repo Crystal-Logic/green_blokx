354f3f4dd894a5a27da7e1a69b6f8b1e81845997
“Nadiya <nadiya.sidelnyk@crystalogic.net>
2022-11-24 13:09:58 +0200

    add partner

--- components/Partners/Partners.tsx
+++ components/Partners/Partners.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Flex } from '@chakra-ui/react';
+import { Box, Heading, Flex, Image } from '@chakra-ui/react';
 
 export const Partners = () => {
   return (
@@ -11,6 +11,27 @@ export const Partners = () => {
           </Heading>
         </Flex>
         <Box w={'150px'} bg="brand.green"></Box>
+      </Flex>
+
+      {/*  */}
+      <Flex h={'100px'} mt={'51px'}>
+        <Flex w={'446px'} h={'full'} alignItems={'center'}>
+          <Box w={'full'} h={0} borderBottom={'2px solid'} borderColor={'white'}></Box>
+          <Box w={'10px'} h={'10px'} bg={'white'} borderRadius={100}></Box>
+        </Flex>
+        <Flex alignItems={'center'}>
+          <Image
+            src={'/images/partners/inspira_wealth.png'}
+            alt="inspira wealth logo"
+            w={'249px'}
+            h={'68px'}
+            mx={'40px'}
+          />
+        </Flex>
+        <Flex flex={'1'} h={'full'} alignItems={'center'}>
+          <Box w={'10px'} h={'10px'} bg={'white'} borderRadius={100}></Box>
+          <Box w={'full'} h={0} borderBottom={'2px solid'} borderColor={'white'}></Box>
+        </Flex>
       </Flex>
     </Box>
   );
