f69c484e9d34479a6fcad30e2a4fc2765e9f51f0
“Nadiya <nadiya.sidelnyk@crystalogic.net>
2022-11-24 13:45:20 +0200

    add partners

--- components/Partners/Partners.tsx
+++ components/Partners/Partners.tsx
@@ -1,8 +1,10 @@
-import { Box, Heading, Flex, Image } from '@chakra-ui/react';
+import { Box, Heading, Flex, Image, useColorModeValue } from '@chakra-ui/react';
 
 export const Partners = () => {
+  const gridColor = useColorModeValue('brand.green', 'white');
+
   return (
-    <Box id="partners" minH={'100vh'}>
+    <Box id="partners" pb={'100px'}>
       <Flex h={'96px'}>
         <Box flex={'1'} bg="brand.green"></Box>
         <Flex w={'400px'} justifyContent="center" alignItems={'center'}>
@@ -13,15 +15,15 @@ export const Partners = () => {
         <Box w={'150px'} bg="brand.green"></Box>
       </Flex>
 
-      {/*  */}
+      {/* Logos grid */}
       <Flex h={'100px'} mt={'51px'}>
-        <Flex w={'446px'} h={'full'} alignItems={'center'}>
-          <Box w={'full'} h={0} borderBottom={'2px solid'} borderColor={'white'}></Box>
-          <Box w={'10px'} h={'10px'} bg={'white'} borderRadius={100}></Box>
+        <Flex w={'306px'} h={'full'} alignItems={'center'}>
+          <Box flex={'1'} h={0} borderBottom={'2px solid'} borderColor={gridColor}></Box>
+          <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
         </Flex>
         <Flex alignItems={'center'}>
           <Image
-            src={'/images/partners/inspira_wealth.png'}
+            src={'/images/partners/inspira_wealth_logo.png'}
             alt="inspira wealth logo"
             w={'249px'}
             h={'68px'}
@@ -29,8 +31,50 @@ export const Partners = () => {
           />
         </Flex>
         <Flex flex={'1'} h={'full'} alignItems={'center'}>
-          <Box w={'10px'} h={'10px'} bg={'white'} borderRadius={100}></Box>
-          <Box w={'full'} h={0} borderBottom={'2px solid'} borderColor={'white'}></Box>
+          <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
+          <Box flex={'1'} h={0} borderBottom={'2px solid'} borderColor={gridColor}></Box>
+        </Flex>
+      </Flex>
+
+      <Flex h={'100px'}>
+        <Flex flex={'1'} h={'full'} alignItems={'center'}>
+          <Box flex={'1'} h={0} borderBottom={'2px solid'} borderColor={gridColor}></Box>
+          <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
+        </Flex>
+        <Flex alignItems={'center'}>
+          <Image src={'/images/partners/aurora_logo.png'} alt="aurora logo" w={'135px'} h={'90px'} mx={'40px'} />
+        </Flex>
+        <Flex w={'546px'} h={'full'} alignItems={'center'}>
+          <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
+          <Box flex={'1'} h={0} borderBottom={'2px solid'} borderColor={gridColor}></Box>
+        </Flex>
+      </Flex>
+
+      <Flex h={'100px'}>
+        <Flex flex={'1'} h={'full'} alignItems={'center'}>
+          <Box flex={'1'} h={0} borderBottom={'2px solid'} borderColor={gridColor}></Box>
+          <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
+        </Flex>
+        <Flex alignItems={'center'}>
+          <Image src={'/images/partners/moonboots_logo.png'} alt="moonboots logo" w={'260px'} h={'45px'} mx={'40px'} />
+        </Flex>
+        <Flex w={'20px'} h={'full'} alignItems={'center'}>
+          <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
+          <Box flex={'1'} h={0} borderBottom={'2px solid'} borderColor={gridColor}></Box>
+        </Flex>
+      </Flex>
+
+      <Flex h={'100px'}>
+        <Flex w={'254px'} h={'full'} alignItems={'center'}>
+          <Box flex={'1'} h={0} borderBottom={'2px solid'} borderColor={gridColor}></Box>
+          <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
+        </Flex>
+        <Flex alignItems={'center'}>
+          <Image src={'/images/partners/near_logo.png'} alt="near logo" w={'215px'} h={'84px'} mx={'40px'} />
+        </Flex>
+        <Flex flex={'1'} h={'full'} alignItems={'center'}>
+          <Box w={'10px'} h={'10px'} bg={gridColor} borderRadius={100}></Box>
+          <Box flex={'1'} h={0} borderBottom={'2px solid'} borderColor={gridColor}></Box>
         </Flex>
       </Flex>
     </Box>
