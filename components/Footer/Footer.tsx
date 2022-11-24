82f563f998302bffbcdd15bada8c24c4db9b03fc
“Nadiya <nadiya.sidelnyk@crystalogic.net>
2022-11-24 09:45:57 +0200

    add footer logo

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text, useColorModeValue } from '@chakra-ui/react';
+import { Flex, Text, Image, useColorModeValue } from '@chakra-ui/react';
 
 export const Footer = () => {
   const bg = useColorModeValue('brand.green', 'white');
@@ -19,9 +19,20 @@ export const Footer = () => {
       <Text fontSize="18px" lineHeight="23px" fontWeight="400">
         2022 GreenBlokx Inc. All rights reserved
       </Text>
-      <Text fontSize="18px" lineHeight="23px" fontWeight="400">
-        by
-      </Text>
+      <Flex h={'53px'} w={'49px'} alignItems={'center'} position={'relative'}>
+        <Text fontSize="18px" lineHeight="23px" fontWeight="400">
+          by{' '}
+        </Text>
+        <Image
+          src="/images/int_logo.png"
+          alt="intencio logo"
+          w={'49px'}
+          h={'53px'}
+          position={'absolute'}
+          top={'6px'}
+          right={'-10px'}
+        />
+      </Flex>
     </Flex>
   );
 };
