705c48c04c9656af84ec01ea934e662d24445e18
“Nadiya <nadiya.sidelnyk@crystalogic.net>
2022-11-25 15:11:56 +0200

    add team card

--- components/Team/Team.tsx
+++ components/Team/Team.tsx
@@ -4,7 +4,7 @@ export const Team = () => {
   return (
     <Box id="team" minH={'1110px'} pt={'53px'} mb={'93px'}>
       <Box h={'full'} w={'full'} position={'relative'}>
-        <Flex h={'100px'} w={'full'} pl={'205px'} position={'absolute'}>
+        <Flex h={'100px'} w={'full'} pl={'205px'} position={'absolute'} zIndex={10}>
           <Flex flex={'1'} alignItems={'center'}>
             <Box h={'0px'} w={'full'} borderBottom={'3px solid'} borderColor={'brand.green'}></Box>
           </Flex>
@@ -14,7 +14,31 @@ export const Team = () => {
             </Heading>
           </Flex>
         </Flex>
-        <Flex h={'106px'} w={'full'} position={'absolute'} top={'700px'}>
+
+        {/* Slider frame */}
+
+        <Flex h={'630px'} w={'full'} pl={'205px'} position={'absolute'} top={'76px'}>
+          <Image src="/images/team/daniel_pal.png" h={'full'} w={'520px'} filter={'grayscale(1)'} />
+          <Box flex={'1'} pl={'42px'}>
+            <Text mt={'350px'} fontSize="32px" lineHeight="40px" fontWeight="700">
+              Daniel Pal
+            </Text>
+            <Text fontSize="24px" lineHeight="30px" fontWeight="500" color={'brand.green'} pt={'21px'}>
+              Chief Executive Officer
+            </Text>
+            <Text fontSize="18px" lineHeight="23px" fontWeight="400" pt={'35px'}>
+              Florin holds a Master's Degree in Chemistry from the West University of Timișoara with 10+ years of
+              experience in Flexible and Rigid PU Foam synthesis and processing...{' '}
+              <Text as={'span'} color={'brand.red'}>
+                read more
+              </Text>
+            </Text>
+          </Box>
+        </Flex>
+
+        {/* Slider frame end  */}
+
+        <Flex h={'106px'} w={'full'} position={'absolute'} top={'600px'}>
           <Flex w={'160px'} bg="brand.green">
             <Image
               src="/images/logo_white.png"
@@ -25,12 +49,12 @@ export const Team = () => {
               h={'88px'}
             />
           </Flex>
-          <Flex w={'550px'}></Flex>
+          <Flex w={'580px'}></Flex>
           <Flex flex={'1'}>
             <Box h={'full'} w={'full'} borderBottom={'3px solid'} borderColor={'brand.green'}></Box>
           </Flex>
         </Flex>
-        <Box position={'absolute'} top={'900px'}>
+        <Box position={'absolute'} top={'800px'}>
           <Button
             variant="outline"
             color="brand.green"
