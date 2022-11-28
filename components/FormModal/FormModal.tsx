f5bf08c7c5d0e7f864360ec1dfad5207c5a20fb6
“Nadiya <nadiya.sidelnyk@crystalogic.net>
2022-11-28 13:15:40 +0200

    split modal

--- components/FormModal/FormModal.tsx
+++ components/FormModal/FormModal.tsx
@@ -8,113 +8,104 @@ import {
   ModalCloseButton,
   useDisclosure,
   Button,
-  Lorem,
   Box,
   FormControl,
   FormLabel,
   Input,
   Text,
   Select,
-} from '@chakra-ui/react'
+  Flex,
+} from '@chakra-ui/react';
 import React from 'react';
 
-
 export const ContactUsModal = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const initialRef = React.useRef(null)
-  const finalRef = React.useRef(null)
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
 
   return (
     <>
       <Button
-            onClick={onOpen}
-            variant="outline"
-            color="brand.green"
-            colorScheme={'brand.green'}
-            borderRadius="none"
-            borderWidth="2px 2px 2px 0px"
-            h={'135px'}
-            minW={'815px'}
-            fontSize="32px"
-            lineHeight="40px"
-            fontWeight="500"
-            position={'relative'}
-            _hover={{
-              color: 'brand.red',
-              colorScheme: 'brand.red',
-              bg: 'transparent',
-            }}
+        onClick={onOpen}
+        variant="outline"
+        color="brand.green"
+        colorScheme={'brand.green'}
+        borderRadius="none"
+        borderWidth="2px 2px 2px 0px"
+        h={'135px'}
+        minW={'815px'}
+        fontSize="32px"
+        lineHeight="40px"
+        fontWeight="500"
+        position={'relative'}
+        _hover={{
+          color: 'brand.red',
+          colorScheme: 'brand.red',
+          bg: 'transparent',
+        }}
       >
-         <Text as={'span'}  position={'absolute'} right={'85px'}>
-              Contact us
-         </Text>
-     </Button>
-      <Modal
-        initialFocusRef={initialRef}
-        finalFocusRef={finalRef}
-        isOpen={isOpen}
-        onClose={onClose}
-        size={'full'}
-      >
+        <Text as={'span'} position={'absolute'} right={'85px'}>
+          Contact us
+        </Text>
+      </Button>
+      <Modal initialFocusRef={initialRef} finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose} size={'full'}>
         <ModalOverlay />
-        <ModalContent>
-
-          <ModalHeader
-             variant="outline"
-             color="brand.green"
-             fontSize="52px"
-             lineHeight="40px"
-             fontWeight="500"
-          >
+        <ModalContent h={'full'}>
+          <Flex h={'full'}>
+            <Box h={'full'} w={'50%'}>
+              <ModalHeader color="brand.green" fontSize="52px" lineHeight="40px" fontWeight="500">
                 Contact us
-          </ModalHeader>
+              </ModalHeader>
 
-          <ModalBody pb={3}>
-            <FormControl>
-              <Input ref={initialRef} placeholder='Email' />
-            </FormControl>
+              <ModalBody pb={3}>
+                <FormControl>
+                  <Input ref={initialRef} placeholder="Email" />
+                </FormControl>
 
-            <FormControl mt={4}>
-              <Input placeholder='Name' />
-            </FormControl>
+                <FormControl mt={4}>
+                  <Input placeholder="Name" />
+                </FormControl>
 
-            <FormControl mt={4}>
-                <Select placeholder='County of residential'>
-                  <option value='option1'>USA</option>
-                  <option value='option2'>Canada</option>
-                  <option value='option3'>Mexico</option>
-                </Select>
-            </FormControl>
-          </ModalBody>
+                <FormControl mt={4}>
+                  <Select placeholder="County of residential">
+                    <option value="option1">USA</option>
+                    <option value="option2">Canada</option>
+                    <option value="option3">Mexico</option>
+                  </Select>
+                </FormControl>
+              </ModalBody>
 
-          <ModalFooter>
-              <Button
-                    onClick={onClose}
-                    variant="outline"
-                    color="brand.green"
-                    colorScheme={'brand.green'}
-                    borderRadius="none"
-                    borderWidth="2px 2px 2px 0px"
-                    h={'135px'}
-                    minW={'815px'}
-                    fontSize="32px"
-                    lineHeight="40px"
-                    fontWeight="500"
-                    position={'relative'}
-                    _hover={{
-                      color: 'brand.red',
-                      colorScheme: 'brand.red',
-                      bg: 'transparent',
-                    }}
-              >
-                 <Text as={'span'}  position={'absolute'} right={'85px'}>
-                      Contact us
-                 </Text>
-             </Button>
-          </ModalFooter>
+              <ModalFooter>
+                <Button
+                  onClick={onClose}
+                  variant="outline"
+                  color="brand.green"
+                  colorScheme={'brand.green'}
+                  borderRadius="none"
+                  borderWidth="2px 2px 2px 0px"
+                  h={'135px'}
+                  minW={'815px'}
+                  fontSize="32px"
+                  lineHeight="40px"
+                  fontWeight="500"
+                  position={'relative'}
+                  _hover={{
+                    color: 'brand.red',
+                    colorScheme: 'brand.red',
+                    bg: 'transparent',
+                  }}
+                >
+                  <Text as={'span'} position={'absolute'} right={'85px'}>
+                    Contact us
+                  </Text>
+                </Button>
+              </ModalFooter>
+            </Box>
+            <Box h={'full'} w={'50%'} bg={` url(/images/background.png)`}></Box>
+          </Flex>
         </ModalContent>
       </Modal>
     </>
-  )
-}
+  );
+};
