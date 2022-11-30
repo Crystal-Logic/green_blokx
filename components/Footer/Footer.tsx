2cc93674c2605ecdc09673a710eeb083da3823b2
“Nadiya <nadiya.sidelnyk@crystalogic.net>
2022-11-30 21:59:58 +0200

    upd footer mobile

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,11 @@
-import { Flex, Text, Image, useColorModeValue } from '@chakra-ui/react';
+import { Flex, Text, Image, useColorModeValue, Hide } from '@chakra-ui/react';
 
 export const Footer = () => {
   const bg = useColorModeValue('brand.green', 'brand.dark');
 
   return (
     <Flex
-      h={'90px'}
+      h={{ base: '52px', md: '90px' }}
       borderTop={'3px solid'}
       borderColor={'brand.green'}
       alignItems={'center'}
@@ -13,24 +13,42 @@ export const Footer = () => {
       color={'white'}
       bg={bg}
     >
-      <Text fontSize="18px" lineHeight="23px" fontWeight="400">
-        Privacy policy
+      <Text
+        fontSize={{ base: '9px', md: '18px' }}
+        lineHeight={{ base: '12px', md: '23px' }}
+        fontWeight="400"
+        order={{ base: 1, md: 'unset' }}
+      >
+        Privacy{' '}
+        <Hide above="md">
+          <br />
+        </Hide>
+        policy
       </Text>
-      <Text fontSize="18px" lineHeight="23px" fontWeight="400">
-        2022 GreenBlokx Inc. All rights reserved
+      <Text
+        fontSize={{ base: '9px', md: '18px' }}
+        lineHeight={{ base: '12px', md: '23px' }}
+        fontWeight="400"
+        order={{ base: 3, md: 'unset' }}
+      >
+        2022 GreenBlokx Inc.{' '}
+        <Hide above="md">
+          <br />
+        </Hide>{' '}
+        All rights reserved
       </Text>
-      <Flex h={'53px'} w={'49px'} alignItems={'center'} position={'relative'}>
-        <Text fontSize="18px" lineHeight="23px" fontWeight="400">
+      <Flex h={'53px'} w={'49px'} alignItems={'center'} position={'relative'} order={{ base: 2, md: 'unset' }}>
+        <Text fontSize={{ base: '9px', md: '18px' }} lineHeight={{ base: '12px', md: '23px' }} fontWeight="400">
           by{' '}
         </Text>
         <Image
           src="/images/int_logo.png"
           alt="intencio logo"
-          w={'49px'}
-          h={'53px'}
+          w={{ base: '25px', md: '49px' }}
+          h={{ base: '22px', md: '53px' }}
           position={'absolute'}
-          top={'6px'}
-          right={'-10px'}
+          top={{ base: '20px', md: '6px' }}
+          right={{ base: '20px', md: '-10px' }}
         />
       </Flex>
     </Flex>
