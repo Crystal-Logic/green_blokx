1a1e3cd0a5bc62de85f0ff050b409a80d965c772
“Nadiya <nadiya.sidelnyk@crystalogic.net>
2022-12-03 20:58:52 +0200

    update green line hack

--- components/Construction/Construction.tsx
+++ components/Construction/Construction.tsx
@@ -14,34 +14,99 @@ export const Construction = () => {
   return (
     <Box id="about_us">
       <Box w={'full'} pl={{ base: 5, lg: 205 }} pt={{ base: 12, lg: 24 }}>
-        <Heading
-          as="h2"
-          fontSize={{ base: '25px', md: '32px' }}
-          lineHeight={{ base: '31px', md: '40px' }}
-          fontWeight="800"
-          pb={{ base: 5, xl: 0 }}
-          display={'flex'}
-          w={'full'}
-        >
-          <Text as={'span'} w={{ base: 'full', xl: 'auto' }}>
-            Intelligent{' '}
-            <Hide above="md">
-              <br />
-            </Hide>
-            Construction Design&nbsp;
-          </Text>
-          for a
-          <Text
-            as={'span'}
-            color="brand.green"
-            flex={'1'}
-            ml={{ base: 2 }}
-            borderBottom={'3px solid'}
-            borderColor={'brand.green'}
+        <Hide below="xl">
+          <Heading
+            as="h2"
+            fontSize={{ base: '25px', md: '32px' }}
+            lineHeight={{ base: '31px', md: '40px' }}
+            fontWeight="800"
+            pb={{ base: 5, xl: 0 }}
+            display={'flex'}
+            w={'full'}
           >
-            Greener Future
-          </Text>
-        </Heading>
+            <Text as={'span'} w={{ base: 'full', xl: 'auto' }}>
+              Intelligent{' '}
+              <Hide above="md">
+                <br />
+              </Hide>
+              Construction Design&nbsp;
+            </Text>
+            for a
+            <Text
+              as={'span'}
+              color="brand.green"
+              flex={'1'}
+              ml={{ base: 2 }}
+              borderBottom={'3px solid'}
+              borderColor={'brand.green'}
+            >
+              Greener Future
+            </Text>
+          </Heading>
+        </Hide>
+        <Box display={{ base: 'none', md: 'block', xl: 'none' }}>
+          <Heading
+            as="h2"
+            fontSize={{ base: '25px', md: '32px' }}
+            lineHeight={{ base: '31px', md: '40px' }}
+            fontWeight="800"
+            pb={{ base: 5, xl: 0 }}
+            display={'flex'}
+            flexDirection={'column'}
+            w={'full'}
+          >
+            <Text as={'span'} w={{ base: 'full', xl: 'auto' }}>
+              Intelligent{' '}
+              <Hide above="md">
+                <br />
+              </Hide>
+              Construction Design&nbsp;
+            </Text>
+            <Text as={'span'} w={'full'} display={'flex'}>
+              for a
+              <Text
+                as={'span'}
+                color="brand.green"
+                flex={'1'}
+                ml={{ base: 2 }}
+                borderBottom={'3px solid'}
+                borderColor={'brand.green'}
+              >
+                Greener Future
+              </Text>
+            </Text>
+          </Heading>
+        </Box>
+        <Hide above="sm">
+          <Heading
+            as="h2"
+            fontSize={{ base: '25px', md: '32px' }}
+            lineHeight={{ base: '31px', md: '40px' }}
+            fontWeight="800"
+            pb={{ base: 5, xl: 0 }}
+            display={'flex'}
+            flexDirection={'column'}
+            w={'full'}
+          >
+            <Text as={'span'} w={{ base: 'full', xl: 'auto' }}>
+              Intelligent <br />
+              Construction Design&nbsp;
+            </Text>
+            <Text as={'span'} w={'full'} display={'flex'}>
+              for a
+              <Text
+                as={'span'}
+                color="brand.green"
+                flex={'1'}
+                ml={{ base: 2 }}
+                borderBottom={'3px solid'}
+                borderColor={'brand.green'}
+              >
+                Greener Future
+              </Text>
+            </Text>
+          </Heading>
+        </Hide>
         <Text
           fontSize={{ base: '14px', md: '22px' }}
           lineHeight={{ base: '18px', md: '28px' }}
