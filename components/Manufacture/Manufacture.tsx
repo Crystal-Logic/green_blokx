c8184f23dad092fbe356c33f334f916d9c7e4e04
“Nadiya <nadiya.sidelnyk@crystalogic.net>
2022-12-14 02:48:36 +0200

    upd manufacture mobile

--- components/Manufacture/Manufacture.tsx
+++ components/Manufacture/Manufacture.tsx
@@ -2,24 +2,30 @@ import { Box, Flex, Text } from '@chakra-ui/react';
 
 export const Manufacture = () => {
   return (
-    <Flex>
-      <Box w={'52%'}>
+    <Flex direction={{ base: 'column', lg: 'row' }}>
+      <Box w={{ lg: '52%' }} my={{ base: 12, lg: 0 }} bg={'rgba(255, 255, 255, 0.3);'}>
         {' '}
         <Text
           flex="1"
-          fontSize={{ base: '14px', md: '25px' }}
-          lineHeight={{ base: '18px', md: '31px' }}
+          fontSize={{ base: '20px', md: '25px' }}
+          lineHeight={{ base: '25px', md: '31px' }}
           fontWeight="500"
           alignSelf={'center'}
           pl={{ base: 5, md: 100 }}
           pr={{ base: 5, md: 20 }}
-          py={{ base: 5, md: 20 }}
+          py={{ base: 16, md: 20 }}
         >
           GreenBLOKX is building the future of the sustainable building. With the assistance of Blockchain GreenBLOKX
           aims to tackle manufacturing’s primary problems. Audibility, accountability, and traceability.
         </Text>
       </Box>{' '}
-      <Box flex={'1'} bg={'rgba(59, 173, 5, 0.4)'} boxShadow={'100px 100px 100px rgba(0, 0, 0, 0.15)'}></Box>
+      <Box
+        h={{ base: '360px' }}
+        w={'full'}
+        flex={{ lg: '1' }}
+        bg={'rgba(59, 173, 5, 0.4)'}
+        boxShadow={'100px 100px 100px rgba(0, 0, 0, 0.15)'}
+      ></Box>
     </Flex>
   );
 };
