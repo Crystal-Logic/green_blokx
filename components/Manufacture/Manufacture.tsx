d89df246b1e4f61ed77014882828a9ecbef3903c
“Nadiya <nadiya.sidelnyk@crystalogic.net>
2022-12-16 07:58:28 +0200

    upd heading

--- components/Manufacture/Manufacture.tsx
+++ components/Manufacture/Manufacture.tsx
@@ -76,55 +76,77 @@ export const Manufacture = () => {
   });
 
   return (
-    <Flex direction={{ base: 'column', lg: 'row' }}>
-      <Box w={{ lg: '52%' }} mb={{ base: 12, lg: 0 }} bg={bgColor}>
-        {' '}
-        <Text
-          flex="1"
-          fontSize={{ base: '20px', md: '25px' }}
-          lineHeight={{ base: '25px', md: '31px' }}
-          fontWeight="500"
-          alignSelf={'center'}
-          pl={{ base: 5, md: 100 }}
-          pr={{ base: 5, md: 20 }}
-          py={{ base: 16, md: 20 }}
-        >
-          GreenBLOKX is building the future of the sustainable building. With the assistance of Blockchain GreenBLOKX
-          aims to tackle manufacturing’s primary problems. Audibility, accountability, and traceability.
-        </Text>
-      </Box>
-      <Box
-        ref={inViewRef}
-        h={{ base: '360px', md: 'auto' }}
-        w={'full'}
-        flex={{ lg: '1' }}
-        boxShadow={'100px 100px 100px rgba(0, 0, 0, 0.15)'}
-        position={'relative'}
-        className={'custom_pointer-cursor'}
+    <Box>
+      <Flex
+        maxW={{ base: 'full', md: '90%', xl: '70%' }}
+        flex={'1'}
+        pl={{ base: 5, md: 44 }}
+        pb={{ base: 5, lg: '50px' }}
+        zIndex={10}
       >
+        <Text
+          alignSelf={'end'}
+          fontSize={{ base: '28px', md: '50px' }}
+          lineHeight={{ base: '35px', md: '63px' }}
+          fontWeight="600"
+        >
+          <Text as={'span'} color="brand.green">
+            Why&nbsp;
+          </Text>
+          Greenblokx?
+        </Text>
+      </Flex>
+
+      <Flex direction={{ base: 'column', lg: 'row' }}>
+        <Box w={{ lg: '52%' }} mb={{ base: 12, lg: 0 }} bg={bgColor}>
+          {' '}
+          <Text
+            flex="1"
+            fontSize={{ base: '20px', md: '25px' }}
+            lineHeight={{ base: '25px', md: '31px' }}
+            fontWeight="500"
+            alignSelf={'center'}
+            pl={{ base: 5, md: 100 }}
+            pr={{ base: 5, md: 20 }}
+            py={{ base: 16, md: 20 }}
+          >
+            GreenBLOKX is building the future of the sustainable building. With the assistance of Blockchain GreenBLOKX
+            aims to tackle manufacturing’s primary problems. Audibility, accountability, and traceability.
+          </Text>
+        </Box>
         <Box
-          ref={videoRef}
-          as="video"
-          muted={true}
-          loop={currStep === 2}
+          ref={inViewRef}
+          h={{ base: '360px', md: 'auto' }}
           w={'full'}
-          onClick={nextStep}
-          onTimeUpdate={pause}
-          src={colorMode === 'dark' ? `/video/dark/${videoSteps[currStep]}` : `/video/white/${videoSteps[currStep]}`}
-        ></Box>
-        <Box
-          ref={infoAreaRef}
-          display={isShowInfoArea ? 'block' : 'none'}
-          position={'absolute'}
-          top={0}
-          right={0}
-          bottom={0}
-          left={0}
-          zIndex={100}
+          flex={{ lg: '1' }}
+          boxShadow={'100px 100px 100px rgba(0, 0, 0, 0.15)'}
+          position={'relative'}
           className={'custom_pointer-cursor'}
-          h={'auto'}
-        ></Box>
-      </Box>
-    </Flex>
+        >
+          <Box
+            ref={videoRef}
+            as="video"
+            muted={true}
+            loop={currStep === 2}
+            w={'full'}
+            onClick={nextStep}
+            onTimeUpdate={pause}
+            src={colorMode === 'dark' ? `/video/dark/${videoSteps[currStep]}` : `/video/white/${videoSteps[currStep]}`}
+          ></Box>
+          <Box
+            ref={infoAreaRef}
+            display={isShowInfoArea ? 'block' : 'none'}
+            position={'absolute'}
+            top={0}
+            right={0}
+            bottom={0}
+            left={0}
+            zIndex={100}
+            className={'custom_pointer-cursor'}
+            h={'auto'}
+          ></Box>
+        </Box>
+      </Flex>
+    </Box>
   );
 };
