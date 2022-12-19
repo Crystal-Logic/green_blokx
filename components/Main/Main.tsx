7eae94dd54846bd6614c26ceb95598bc993e0838
“Nadiya <nadiya.sidelnyk@crystalogic.net>
2022-12-19 04:00:58 +0200

    add main bg cube block

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -10,9 +10,16 @@ import {
   VStack,
   Button,
   useColorModeValue,
+  keyframes,
 } from '@chakra-ui/react';
 import { useInView } from 'react-intersection-observer';
 import { NavBar } from '../NavBar';
+import { CubeGif } from 'components/CubeGif/CubeGif';
+
+const spin = keyframes`
+  from {transform: rotate(0deg);}
+  to {transform: rotate(360deg)}
+`;
 
 export const Main = ({ onOpenModal }: { onOpenModal: () => void }) => {
   const [isShowVideo, setIsShowVideo] = useState(false);
@@ -62,7 +69,12 @@ export const Main = ({ onOpenModal }: { onOpenModal: () => void }) => {
   return (
     <Flex minH="100vh" w={'full'} ref={inViewRef}>
       <Flex flex={'1'} direction={'column'} position={'relative'} backgroundSize="cover">
-        <Box pt={{ base: '40px', lg: '50px' }}>
+        <Box
+          pt={{ base: '40px', lg: '50px' }}
+          position={'relative'}
+          zIndex={20}
+          visibility={isShowVideo ? 'hidden' : 'initial'}
+        >
           <NavBar />
         </Box>
         <video
@@ -102,8 +114,8 @@ export const Main = ({ onOpenModal }: { onOpenModal: () => void }) => {
             ) : (
               <Image
                 src={pauseIcon}
-                w={{ base: '26px', md: '35px' }}
-                h={{ base: '25px', md: '32px' }}
+                w={{ base: 'full', md: '35px' }}
+                h={{ base: 'full', md: '32px' }}
                 className={'custom_pointer-cursor'}
               ></Image>
             )}
@@ -132,6 +144,122 @@ export const Main = ({ onOpenModal }: { onOpenModal: () => void }) => {
             </Text>
           </VStack>
         </Hide>
+
+        {/* Cubes layout */}
+        <Box
+          pos={'absolute'}
+          top={0}
+          bottom={0}
+          right={0}
+          left={0}
+          overflowX={'hidden'}
+          display={isShowVideo ? 'none' : 'block'}
+        >
+          <CubeGif
+            w={'140px'}
+            h={'140px'}
+            position={'absolute'}
+            left={'13%'}
+            top={'10%'}
+            transform="rotate(-15.89deg)"
+          />
+          <CubeGif
+            w={'60px'}
+            h={'60px'}
+            position={'absolute'}
+            left={'40px'}
+            top={'135px'}
+            transform="rotate(20.59deg)"
+          />
+          <CubeGif
+            w={'100px'}
+            h={'100px'}
+            position={'absolute'}
+            left={'50px'}
+            top={'190px'}
+            transform="rotate(-11.93deg)"
+          />
+          <CubeGif
+            w={'120px'}
+            h={'120px'}
+            position={'absolute'}
+            left={'80px'}
+            top={'40%'}
+            transform="rotate(21.31deg)"
+          />
+          <CubeGif
+            w={'60px'}
+            h={'60px'}
+            position={'absolute'}
+            left={'140px'}
+            top={'72%'}
+            transform="rotate(-11.66deg)"
+          />
+          <CubeGif
+            w={'60px'}
+            h={'60px'}
+            position={'absolute'}
+            left={'300px'}
+            top={'68%'}
+            transform="rotate(-17.84deg)"
+          />
+          <CubeGif
+            w={'150px'}
+            h={'150px'}
+            position={'absolute'}
+            left={'400px'}
+            top={'72%'}
+            transform="rotate(2.85deg)"
+          />
+          {/* right part */}
+          <CubeGif w={'80px'} h={'80px'} position={'absolute'} left={'68%'} top={'8%'} transform="rotate(25.88deg)" />
+          <CubeGif w={'60px'} h={'60px'} position={'absolute'} left={'77%'} top={'16%'} transform="rotate(8.41deg)" />
+          <CubeGif w={'70px'} h={'70px'} position={'absolute'} left={'82%'} top={'26%'} transform="rotate(8.41deg)" />
+          <CubeGif w={'150px'} h={'150px'} position={'absolute'} left={'87%'} top={'18%'} transform="rotate(8.63deg)" />
+          <CubeGif
+            w={'180px'}
+            h={'180px'}
+            position={'absolute'}
+            left={'70%'}
+            top={'30%'}
+            transform="rotate(1.08deg)"
+            animation={`${spin} 20s infinite linear`}
+          />
+          <CubeGif w={'70px'} h={'70px'} position={'absolute'} left={'92%'} top={'42%'} transform="rotate(8.41deg)" />
+          <CubeGif
+            w={'130px'}
+            h={'130px'}
+            position={'absolute'}
+            left={'84%'}
+            top={'50%'}
+            transform="rotate(158.3deg)"
+          />
+          <CubeGif w={'100px'} h={'100px'} position={'absolute'} left={'72%'} top={'60%'} transform="rotate(8.41deg)" />
+          <CubeGif
+            w={'100px'}
+            h={'100px'}
+            position={'absolute'}
+            left={'91%'}
+            top={'65%'}
+            transform="rotate(-11.93deg)"
+          />
+          <CubeGif
+            w={'100px'}
+            h={'100px'}
+            position={'absolute'}
+            left={'60%'}
+            top={'80%'}
+            transform="rotate(-5.03deg)"
+          />
+          <CubeGif
+            w={'180px'}
+            h={'180px'}
+            position={'absolute'}
+            left={'74%'}
+            top={'74%'}
+            transform="rotate(-10.59deg)"
+          />
+        </Box>
       </Flex>
     </Flex>
   );
